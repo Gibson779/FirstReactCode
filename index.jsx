a89e28d62424e88a82c907eb086ea8ba23344ec1
rgib <regangib@btinternet.com>
2025-10-04 11:39:28 +0100

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -1,6 +1,14 @@
+import { createElement } from "react"
 import { createRoot } from "react-dom/client"
 
-const root = createRoot(document.querySelector("#root"))
-root.render(<p>Hello from the world of React!</p>)
+const root = createRoot(document.getElementById("root"))
+const reactElement = createElement("h1", null, "Hello from createElement!")
+
+console.log(reactElement)
+
+root.render(
+    reactElement
+)
+
 
 
