f1b5f81160d2d2bd580008b5a72438becc07d606
rgib <regangib@btinternet.com>
2025-10-05 12:00:36 +0100

    New Commit

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client"
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+const root = createRoot(document.getElementById("root"))
+
+
+root.render(
+  <main>
+    <img src="React-icon.png" width="40px"/>
+    <h1>Fun Facts abount React</h1>
+   <ul>
+    <li>Was first rleased in 2013</li>
+    <li>was creacted</li>
+    <li>Has over 100k stars</li>
+    <li>mainteined by Meta</li> 
+    <li>Powers apps and mobile apps</li>
+    </ul>
+  </main>
 )
