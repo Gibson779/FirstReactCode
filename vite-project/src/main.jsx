668145587138264baad947061df4c3a76f85a6a8
rgib <regangib@btinternet.com>
2025-10-06 20:01:13 +0100

    Update

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,19 +1,12 @@
-import { createRoot } from "react-dom/client"
 
+import {createRoot} from "react-dom/client"
+const root = createRoot(document.getElementById)("root")
 
-const root = createRoot(document.getElementById("root"))
+function tempName (){
 
-
-root.render(
-  <main>
-    <img src="React-icon.png" width="40px"/>
-    <h1>Fun Facts abount React</h1>
-   <ul>
-    <li>Was first rleased in 2013</li>
-    <li>was creacted</li>
-    <li>Has over 100k stars</li>
-    <li>mainteined by Meta</li> 
-    <li>Powers apps and mobile apps</li>
-    </ul>
-  </main>
-)
+  return(
+    <main>
+      <img src="React-icon.png"
+    </main>
+  )
+}
