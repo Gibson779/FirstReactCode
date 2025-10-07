7a980bf89259fdcb3230de4b82787a91cc82766f
rgib <regangib@btinternet.com>
2025-10-07 20:47:28 +0100

    Update latest

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,25 +1,16 @@
-import { createRoot } from "react-dom/client"
-const root = createRoot(document.getElementById("root"))
+import {createRoot} from "react-dom/client"
+const root = createRoot(document.getElementById("Root"))
 
-function TemporaryName() {
-    return (
-        <main>
-            <img src="react-logo.png" width="40px" alt="React logo" />
-            <h1>Fun facts about React!</h1>
-            <ol>
-                <li>Was first release in 2013</li>
-                <li>Was originally created by Jordan Walke</li>
-                <li>Has well over 200K stars on GitHub</li>
-                <li>Is maintained by Meta</li>
-                <li>Powers thousands of enterprise apps, including mobile apps</li>
-            </ol>
-        </main>
-    )
-
-    
+function MyList(){
+  return(
+    <ol>
+      <li>
+        My is is looking good now
+      </li>
+    </ol>
+  )
 }
 
-
 root.render(
-    <TemporaryName/>
+  <MyList />
 )
