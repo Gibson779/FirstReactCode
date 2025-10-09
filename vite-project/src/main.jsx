b58b6c2a10d3de0038876992bbd2a4a614e9b1bb
rgib <regangib@btinternet.com>
2025-10-09 13:44:27 +0100

    Update main.jsx

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,16 +1,31 @@
 import {createRoot} from "react-dom/client"
-const root = createRoot(document.getElementById("Root"))
+const root = createRoot(document.getElementById("root"))
 
-function MyList(){
+function ShoppingList(){
   return(
+    <div>
+      <header>
+         <img src="React-icon.png" alt="React Logo" width="40px"/>
+      </header>
+      <main>
+      <h1>My Shopping List </h1>
     <ol>
-      <li>
-        My is is looking good now
-      </li>
-    </ol>
+          <li> Bread </li>
+          <li> Milk </li>
+          <li> eggs </li>
+          <li> fruit </li>
+       </ol>
+    </main>
+    <footer>
+        <small>
+           © copyright 2024 Gibson developer. All rights reserved.
+        </small>
+      </footer>
+    </div>
+
+    
   )
 }
-
 root.render(
-  <MyList />
+  < ShoppingList />
 )
