eb01808f50eb1fd54a41c15e10099caa1b3501e7
rgib <regangib@btinternet.com>
2025-10-15 18:06:32 +0100

    Latest Commit

---
+++ vite-project/src/component/Navbar.jsx
@@ -0,0 +1,7 @@
+export default function Navbar (){
+  return(
+<main>
+      <h1 Navbar>Nav bar goes here </h1>
+    </main>
+  )
+}
--- vite-project/src/index.jsx
+++ vite-project/src/index.jsx
@@ -1,23 +1,10 @@
 import {createRoot} from "react-dom/client"
 const root = createRoot(document.getElementById("root"))
+
 import '../src/app.css';
-import Header from "./Header"
-import Footer from "./Footer"
-import Main from "./Main"
+import App from "./App"
 
-
-
-function Page(){
-  return(
-    <>
-    <Header/>
-    <Main/>
-    <Footer/>
-    </>
-  )
-}
-  
 
 root.render(
-  < Page />
+  < App />
 )
